333fbcc5f2b88f3d7e6d269e86461b37793548af
Eleansphere <franek.vojtech01@gmail.com>
2024-03-16 22:02:43 +0100

    upd9

--- script.js
+++ script.js
@@ -3,30 +3,24 @@ const ratingButtons = document.querySelectorAll('input');
 const ratingValue = document.getElementById('rating-value');
 const submitBtn = document.getElementById('submitBtn');
 const sections = document.querySelectorAll('section');
-
 ratingValue.innerText = 0;
-
-//control click on radio
-  ratingButtons.forEach((item)=>{
-    item.addEventListener("click", ()=>{
-//inserting value
-      ratingValue.innerText = item.value;
-    });
-  });
   // changing the page
   submitBtn.addEventListener("click",()=>{
-    if (
-      ratingValue.innerText != 0
-    ) {
-      sections[0].style.display = 'none';
-      sections[1].style.display = 'flex';
-      //resetting checked state before reload
       ratingButtons.forEach((item)=>{
-        item.checked = false;
+        if(item.checked == true){
+          ratingValue.innerText = item.value;
+        }
       });
-      setInterval(()=>{location.reload()},4000);
-    } else {
-      submitBtn.disabled = true;      
-      setInterval(()=>{submitBtn.disabled = false}, 3500);
-    }
+        if(ratingValue.innerText != 0){
+          sections[0].style.display = 'none';
+          sections[1].style.display = 'flex';
+          //resetting checked state before reload
+          ratingButtons.forEach((item)=>{
+            item.checked == true ? item.checked = false : null;
+          });
+          setInterval(()=>{location.reload()},4000);
+        } else {
+          submitBtn.disabled = true;      
+          setInterval(()=>{submitBtn.disabled = false}, 3500);
+        }
   });
